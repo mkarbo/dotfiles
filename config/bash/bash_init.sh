7eeb084f319a318f0ad987f43b9dbab79f2122a4
Malthe <mkar@migration-vm2.u5dhgopl3lquvlnptexikavfwa.ax.internal.cloudapp.net>
2019-05-16 08:21:46 +0000

    added count function

--- config/bash/bash_init.sh
+++ config/bash/bash_init.sh
@@ -10,3 +10,15 @@ alias vim='nvim'
 PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '
 force_color_prompt=yes
 
+function count() {
+DIRPARAM=$1
+TYPEPARAM=${2:-f}
+numfiles="$(find ./${DIRPARAM} -type ${TYPEPARAM} | wc | awk '{print $1}')"
+if [ $TYPEPARAM == f ]; then 
+	echo -e "number of files in directory is: \e[96m$numfiles\e[0m"
+
+else
+	echo -e "number of type ${TYPEPARAM} in directory is: \e[96m$numfiles\e[0m"
+fi
+}
+
