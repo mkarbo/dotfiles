bb0799023737bafd1ab715174d059f3f41bf1228
Malthe Karbo <malthe@resights.dk>
2022-03-29 20:08:37 +0200

    Added some configuration and octo support

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -161,11 +161,23 @@ lvim.plugins = {
 	},
 	{ "nvim-telescope/telescope-project.nvim" },
 	{ "nvim-telescope/telescope-file-browser.nvim" },
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("user.octo").setup()
+		end,
+	},
 }
 
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("file_browser")
 require("telescope").load_extension("project")
+require("telescope").load_extension("octo")
 
 require("dapui").setup()
 
@@ -237,3 +249,145 @@ dap.configurations.python = {
 		pythonPath = default_setup_opts.pythonPath,
 	},
 }
+
+require("user.octo").setup()
+
+local octo_function_map = require("octo.mappings").keypress_event_cbs
+local octo_new_mappings = {
+	issue = {
+		close_issue = "ic", -- close issue
+		reopen_issue = "io", -- reopen issue
+		list_issues = "il", -- list open issues on same repo
+		reload = "<C-r>", -- reload issue
+		open_in_browser = "<C-b>", -- open issue in browser
+		copy_url = "<C-y>", -- copy url to system clipboard
+		add_assignee = "aa", -- add assignee
+		remove_assignee = "ad", -- remove assignee
+		create_label = "lc", -- create label
+		add_label = "la", -- add label
+		remove_label = "ld", -- remove label
+		goto_issue = "gi", -- navigate to a local repo issue
+		add_comment = "ca", -- add comment
+		delete_comment = "cd", -- delete comment
+		next_comment = "]c", -- go to next comment
+		prev_comment = "[c", -- go to previous comment
+		react_hooray = "rp", -- add/remove 🎉 reaction
+		react_heart = "rh", -- add/remove ❤️ reaction
+		react_eyes = "re", -- add/remove 👀 reaction
+		react_thumbs_up = "r+", -- add/remove 👍 reaction
+		react_thumbs_down = "r-", -- add/remove 👎 reaction
+		react_rocket = "rr", -- add/remove 🚀 reaction
+		react_laugh = "rl", -- add/remove 😄 reaction
+		react_confused = "rc", -- add/remove 😕 reaction
+	},
+	pull_request = {
+		checkout_pr = "po", -- checkout PR
+		merge_pr = "pm", -- merge PR
+		list_commits = "pc", -- list PR commits
+		list_changed_files = "pf", -- list PR changed files
+		show_pr_diff = "pd", -- show PR diff
+		add_reviewer = "va", -- add reviewer
+		remove_reviewer = "vd", -- remove reviewer request
+		close_issue = "ic", -- close PR
+		reopen_issue = "io", -- reopen PR
+		list_issues = "il", -- list open issues on same repo
+		reload = "<C-r>", -- reload PR
+		open_in_browser = "<C-b>", -- open PR in browser
+		copy_url = "<C-y>", -- copy url to system clipboard
+		add_assignee = "aa", -- add assignee
+		remove_assignee = "ad", -- remove assignee
+		create_label = "lc", -- create label
+		add_label = "la", -- add label
+		remove_label = "ld", -- remove label
+		goto_issue = "gi", -- navigate to a local repo issue
+		add_comment = "ca", -- add comment
+		delete_comment = "cd", -- delete comment
+		next_comment = "]c", -- go to next comment
+		prev_comment = "[c", -- go to previous comment
+		react_hooray = "rp", -- add/remove 🎉 reaction
+		react_heart = "rh", -- add/remove ❤️ reaction
+		react_eyes = "re", -- add/remove 👀 reaction
+		react_thumbs_up = "r+", -- add/remove 👍 reaction
+		react_thumbs_down = "r-", -- add/remove 👎 reaction
+		react_rocket = "rr", -- add/remove 🚀 reaction
+		react_laugh = "rl", -- add/remove 😄 reaction
+		react_confused = "rc", -- add/remove 😕 reaction
+	},
+	review_thread = {
+		goto_issue = "gi", -- navigate to a local repo issue
+		add_comment = "ca", -- add comment
+		add_suggestion = "sa", -- add suggestion
+		delete_comment = "cd", -- delete comment
+		next_comment = "]c", -- go to next comment
+		prev_comment = "[c", -- go to previous comment
+		select_next_entry = "]q", -- move to previous changed file
+		select_prev_entry = "[q", -- move to next changed file
+		close_review_tab = "<C-c>", -- close review tab
+		react_hooray = "rp", -- add/remove 🎉 reaction
+		react_heart = "rh", -- add/remove ❤️ reaction
+		react_eyes = "re", -- add/remove 👀 reaction
+		react_thumbs_up = "r+", -- add/remove 👍 reaction
+		react_thumbs_down = "r-", -- add/remove 👎 reaction
+		react_rocket = "rr", -- add/remove 🚀 reaction
+		react_laugh = "rl", -- add/remove 😄 reaction
+		react_confused = "rc", -- add/remove 😕 reaction
+	},
+	submit_win = {
+		approve_review = "<C-a>", -- approve review
+		comment_review = "<C-m>", -- comment review
+		request_changes = "<C-r>", -- request changes review
+		close_review_tab = "<C-c>", -- close review tab
+	},
+	review_diff = {
+		add_review_comment = "ca", -- add a new review comment
+		add_review_suggestion = "sa", -- add a new review suggestion
+		focus_files = "F", -- move focus to changed file panel
+		toggle_files = "f", -- hide/show changed files panel
+		next_thread = "]]", -- move to next thread
+		prev_thread = "[[", -- move to previous thread
+		select_next_entry = "]q", -- move to previous changed file
+		select_prev_entry = "[q", -- move to next changed file
+		close_review_tab = "<C-c>", -- close review tab
+		toggle_viewed = "<space>", -- toggle viewer viewed state
+	},
+	file_panel = {
+		next_entry = "j", -- move to next changed file
+		prev_entry = "k", -- move to previous changed file
+		select_entry = "<cr>", -- show selected changed file diffs
+		refresh_files = "R", -- refresh changed files panel
+		focus_files = "F", -- move focus to changed file panel
+		toggle_files = "f", -- hide/show changed files panel
+		select_next_entry = "]q", -- move to previous changed file
+		select_prev_entry = "[q", -- move to next changed file
+		close_review_tab = "<C-c>", -- close review tab
+		toggle_viewed = "<space>", -- toggle viewer viewed state
+	},
+}
+
+local section_mappers = {
+	issue = "i",
+	pull_request = "p",
+	review_thread = "r",
+	submit_win = "s",
+	review_diff = "d",
+	file_panel = "f",
+}
+
+local octo_table = {
+	name = "+Octo",
+}
+for s_name, pref in pairs(section_mappers) do
+	local _section = octo_new_mappings[s_name]
+	local _table = { name = s_name }
+	for func_name, keybind in pairs(_section) do
+		local bind = "<cmd>" .. "lua require('octo.mappings').keypress_event_cbs['" .. func_name .. "']()<CR>"
+
+		_table[keybind] = {
+			bind,
+			s_name .. ": " .. func_name,
+		}
+	end
+	octo_table[pref] = _table
+end
+
+lvim.builtin.which_key.mappings["zo"] = octo_table
